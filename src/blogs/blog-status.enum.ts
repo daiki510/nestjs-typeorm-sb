6c17bc38455bb36cefd572ce0c47da62080e223f
daiki510 <ddgg7755@gmail.com>
2022-03-28 22:30:51 +0900

    create entity

---
+++ src/blogs/blog-status.enum.ts
@@ -0,0 +1,4 @@
+export enum BlogStatus {
+  PUBLISH = 1,
+  DRAFT = 2,
+}
---
+++ src/entities/blog.entity.ts
@@ -0,0 +1,43 @@
+import { BlogStatus } from 'src/blogs/blog-status.enum';
+import {
+  Entity,
+  Column,
+  Index,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity()
+export class BlogEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({
+    nullable: false,
+    comment: '記事タイトル',
+  })
+  title: string;
+
+  @Column({
+    type: 'text',
+    nullable: true,
+    comment: '記事内容',
+  })
+  content: string;
+
+  @Column({
+    type: 'enum',
+    enum: BlogStatus,
+    default: BlogStatus.DRAFT,
+    comment: '公開状況',
+  })
+  status: BlogStatus;
+
+  @Index('createdAt-idx')
+  @CreateDateColumn()
+  readonly createdAt: string;
+
+  @UpdateDateColumn()
+  readonly updatedAt: string;
+}
